b43b66fb077467f79c6733da19edb80afb7440fb
Eltayeb Hassan <ms.eme2011@gmail.com>
2018-10-14 14:00:33 +0200

    Fix E15

--- app/Http/Controllers/API/E15.php
+++ app/Http/Controllers/API/E15.php
@@ -20,20 +20,20 @@ use App\Model\Payment\E15 as E15Model;
 class E15 extends Controller
 {
     //
-    public function e15(Request $request,$type)
+    public function e15(Request $request, $type)
     {
         if ($request->isJson()) {
             $token = JWTAuth::parseToken();
             $user = $token->authenticate();
 
-            $validator = Validator::make($request->all(),[
+            $validator = Validator::make($request->all(), [
                     'phone' => 'required|numeric',
                     'IPIN' => 'required|numeric|digits_between:4,4',
                     'amount' => 'required|numeric',
                     'invoiceNo' => 'required|numeric',
                 ]
             );
-            if ($validator->fails()){
+            if ($validator->fails()) {
                 return response()->json([
                     'error' => true,
                     'errors' => $validator->errors()->toArray()
@@ -47,18 +47,18 @@ class E15 extends Controller
             //$user = JWTAuth::toUser($token);
             /******   Create Transaction Object  *********/
             $phone = $request->json()->get("phone");
-            $amount =  $request->json()->get("amount");
-            $amount =number_format((float)$amount, 2, '.', '');
+            $amount = $request->json()->get("amount");
+            $amount = number_format((float)$amount, 2, '.', '');
 
             $ipin = $request->json()->get("IPIN");
             $invoice = $request->json()->get("invoiceNo");
 
             $bank = Functions::getBankAccountByUser($user);
-            if ($ipin !== $bank->IPIN){
+            if ($ipin !== $bank->IPIN) {
                 $response = array();
                 $response = ["error" => true];
                 $response = ["message" => "Wrong IPIN Code"];
-                return response()->json($response,200);
+                return response()->json($response, 200);
             }
 
             $transaction = new Transaction();
@@ -93,58 +93,55 @@ class E15 extends Controller
 
             $publickKey = PublicKey::sendRequest();
             //dd($ipin);
-            if ($publickKey == false){
+            if ($publickKey == false) {
                 $res = array();
                 $res += ["error" => true];
                 $res += ["message" => "Server Error"];
-                return response()->json($res,200);
+                return response()->json($res, 200);
             }
-            $ipin = Functions::encript($publickKey , $uuid , $ipin);
+            $ipin = Functions::encript($publickKey, $uuid, $ipin);
 
 
             //$req = E15Model::requestBuild($transaction->id,$ipin,$type);
-            $response = E15Model::sendRequest($transaction->id,$ipin,$type);
+            $response = E15Model::sendRequest($transaction->id, $ipin, $type);
             if ($response == false) {
                 $res = array();
                 $res += ["error" => true];
                 $res += ["message" => "Some Error Found"];
-                return response()->json($res,200);
+                return response()->json($res, 200);
             }
-            if ($response->responseCode != 0){
+            if ($response->responseCode != 0) {
                 $response_json = array();
                 $response_json += ["error" => true];
                 $response_json += ["message" => "Server error"];
-                return response()->json($response_json,200);
+                return response()->json($response_json, 200);
 
             }
 
             $basicResonse = Response::saveBasicResponse($transaction, $response);
 
-            $paymentResponse = PaymentResponse::savePaymentResponse($basicResonse, $payment , $response);
-            self::saveE15Response($paymentResponse,$e15,$response);
+            $paymentResponse = PaymentResponse::savePaymentResponse($basicResonse, $payment, $response);
+            self::saveE15Response($paymentResponse, $e15, $response);
 
 
             $json = array();
-            $bill_info=$response->billInfo;
+            $bill_info = $response->billInfo;
             $invoice_status = $bill_info->invoiceStatus;
             $status = "";
-            if ($invoice_status == 0){
+            if ($invoice_status == 0) {
                 $status = "CANCELED";
-            }
-            else if ($invoice_status == 1){
+            } else if ($invoice_status == 1) {
                 $status = "PENDING";
-            }
-            else{
+            } else {
                 $status = "PAID";
             }
             $json += ["error" => false];
             $json += ["message" => "Done Successfully"];
             $json += ["status" => $status];
             $json += ["response" => $bill_info];
-            return response()->json($json,200);
+            return response()->json($json, 200);
 
             //return response()->json($json,200);
-
 
 
         } else {
@@ -154,20 +151,34 @@ class E15 extends Controller
             return response()->json($response, 200);
         }
     }
-    public function e15_payment(Request $request){
-        return $this->e15($request,6);
+
+    public function e15_payment(Request $request)
+    {
+        return $this->e15($request, 6);
     }
-    public function e15_inquery(Request $request){
-        return $this->e15($request,2);
+
+    public function e15_inquery(Request $request)
+    {
+        return $this->e15($request, 2);
     }
-    public static function saveE15Response( $paymentResponse ,  $e15 , $response){
+
+    public static function saveE15Response($paymentResponse, $e15, $response)
+    {
         $e15_response = new E15Response();
         $e15_response->PaymentResponse()->associate($paymentResponse);
         $e15_response->E15()->associate($e15);
-        $bill_info=$response->billInfo;
-        $e15_response->invoice_no = $bill_info->invoiceNo;
-        $e15_response->expiry = $bill_info->invoiceExpiryDate;
-        $e15_response->status = $bill_info->invoiceStatus;
+        $bill_info = $response->billInfo;
+        $e15_response->UnitName = $bill_info->UnitName;
+        $e15_response->ServiceName = $bill_info->ServiceName;
+        $e15_response->TotalAmount = $bill_info->TotalAmount;
+        $e15_response->ReferenceId = $bill_info->ReferenceId;
+        $e15_response->PayerName = $bill_info->PayerName;
+        //	UnitName	ServiceName	TotalAmount	ReferenceId	PayerName
+
+
+//        $e15_response->invoice_no = $bill_info->invoiceNo;
+//        $e15_response->expiry = $bill_info->invoiceExpiryDate;
+//        $e15_response->status = $bill_info->invoiceStatus;
         $e15_response->save();
     }
 }
