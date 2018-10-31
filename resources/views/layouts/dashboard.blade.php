71bc6bf956bc09eecd47bed647c27d89583eb743
Eltayeb Hassan <Mr.elteb@gmail.com>
2018-10-31 16:26:00 +0200

    layout fix missing js and css file & add cdn out source

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,22 +1,29 @@
-
 <!DOCTYPE html>
 <html>
 <head>
 
     <title>E-Pay Dashboard</title>
-    <link href="{{asset('css/app.css')}}">
-    {{--mix.js('resources/js/app.js', 'public/js')--}}
-    <link href="{{ asset ('css/bootstrap.min.css')}}" rel="stylesheet">
+
+    {{--    <link href="{{ asset ('css/bootstrap.min.css')}}" rel="stylesheet">--}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-{{--    <link href="{{ asset ('css/sb-admin.css')}}" rel="stylesheet">--}}
-    https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css
+    {{--    <link href="{{ asset ('css/sb-admin.css')}}" rel="stylesheet">--}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css "
+          rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="{{ asset ('css/plugins/morris.css')}}" rel="stylesheet">
-    <link href="{{asset ('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset ('css/datepicker.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset ('css/marris.css')}}" rel="stylesheet">
+    {{--<link href="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js" rel="stylesheet">--}}
+    {{--    <link href="{{asset ('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">--}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet"
+          type="text/css">
+    <link href="https://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css" rel="stylesheet"
+          type="text/css">
+    {{--    <link href="{{asset ('css/datepicker.css')}}" rel="stylesheet" type="text/css">--}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.min.css"
+          rel="stylesheet" type="text/css">
 
 </head>
 <body>
@@ -35,86 +42,89 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="APanel">Dashboard</a>
-                </div>
-                <!-- Top Menu Items -->
-                <ul class="nav navbar-right top-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Admin <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                            </li>
+        </div>
+        <!-- Top Menu Items -->
+        <ul class="nav navbar-right top-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Admin <b
+                            class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                    </li>
 
 
-                            <li class="divider"></li>
-                            <li>
-                                <a href="{{url('/logout')}}"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                            </li>
-                        </ul>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="{{url('/logout')}}"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                     </li>
                 </ul>
-                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav side-nav">
+            </li>
+        </ul>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
 
-                        <li class="{{$active == "home" ? "active" : ""}}">
-                            <a href="{{url ('/home')}}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                        </li>
-                        <li class="{{$active == "bank" ? "active" : ""}}">
-                            <a href="pages/bank.jsp"><i class="fa fa-bank"></i> Manage Banks</a>
-                        </li>
-                        <li class="{{$active == "bank_branch" ? "active" : ""}}">
-                            <a href="pages/bank_branch.jsp"><i class="fa fa-bank"></i> Manage Branches</a>
-                        </li>
-                        <li class="{{$active == "bank_account" ? "active" : ""}}">
-                            <a href="pages/merchant_bank_account.jsp"><i class="fa fa-bank"></i> Manage Banks Accounts</a>
-                        </li>
-                        <li class="{{$active == "merchant" ? "active" : ""}}">
-                            <a href="{{url('/merchants')}}"><i class="fa fa-cc"></i> Manage Merchants</a>
-                        </li>
-                        <li class="{{$active == "merchant_services" ? "active" : ""}}">
-                            <a href="{{url("/services")}}"><i class="fa fa-cogs"></i> Manage Services</a>
-                        </li>
-                        <li class="{{$active == "user" ? "active" : ""}}">
-                            <a href="pages/manage_users.jsp"><i class="fa fa-users"></i> Manage Users</a>
-                        </li>
-                        <li class="{{$active == "transaction" ? "active" : ""}}">
-                            <a href="{{url('/transactions')}}"><i class="fa fa-cog"></i> Transactions</a>
-                        </li>
+                <li class="{{$active == "home" ? "active" : ""}}">
+                    <a href="{{url ('/home')}}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                </li>
+                <li class="{{$active == "bank" ? "active" : ""}}">
+                    <a href="pages/bank.jsp"><i class="fa fa-bank"></i> Manage Banks</a>
+                </li>
+                <li class="{{$active == "bank_branch" ? "active" : ""}}">
+                    <a href="pages/bank_branch.jsp"><i class="fa fa-bank"></i> Manage Branches</a>
+                </li>
+                <li class="{{$active == "bank_account" ? "active" : ""}}">
+                    <a href="pages/merchant_bank_account.jsp"><i class="fa fa-bank"></i> Manage Banks Accounts</a>
+                </li>
+                <li class="{{$active == "merchant" ? "active" : ""}}">
+                    <a href="{{url('/merchants')}}"><i class="fa fa-cc"></i> Manage Merchants</a>
+                </li>
+                <li class="{{$active == "merchant_services" ? "active" : ""}}">
+                    <a href="{{url("/services")}}"><i class="fa fa-cogs"></i> Manage Services</a>
+                </li>
+                <li class="{{$active == "user" ? "active" : ""}}">
+                    <a href="pages/manage_users.jsp"><i class="fa fa-users"></i> Manage Users</a>
+                </li>
+                <li class="{{$active == "transaction" ? "active" : ""}}">
+                    <a href="{{url('/transactions')}}"><i class="fa fa-cog"></i> Transactions</a>
+                </li>
 
-                        <li>
-                            <a href="pages/Report.jsp"><i class="fa fa-file-text-o"></i> Reports</a>
-                        </li>
+                <li>
+                    <a href="pages/Report.jsp"><i class="fa fa-file-text-o"></i> Reports</a>
+                </li>
 
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </nav>
-
-            <div id="page-wrapper">
-
-                <div class="container" style="margin-top: 5%; width: 90%;">
-
-                    <!-- Page Heading -->
-                    @yield('content')
-                </div>
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- /#page-wrapper -->
-
+            </ul>
         </div>
+        <!-- /.navbar-collapse -->
+    </nav>
 
-        <!-- JS Include -->
+    <div id="page-wrapper">
+
+        <div class="container" style="margin-top: 5%; width: 90%;">
+
+            <!-- Page Heading -->
+            @yield('content')
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+
+<!-- JS Include -->
 <script src="{{asset ('js/app.js')}}"></script>
-<script src="{{asset ('js/jquery.js')}}"></script>
-<script src="{{asset ('js/bootstrap.js')}}"></script>
-<script src ="{{asset ('js/jquery.dataTables.min.js')}}"></script>
-<script src ="{{asset ('js/dataTables.bootstrap.min.js')}}"></script>
+{{--<script src="{{asset ('js/bootstrap.js')}}"></script>--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+{{--<script src="{{asset ('js/jquery.dataTables.min.js')}}"></script>--}}
+<script src="https://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+{{--<script src="{{asset ('js/dataTables.bootstrap.min.js')}}"></script>--}}
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#dataTable').DataTable();
     });
 </script>
-    </body>
+</body>
 </html>
