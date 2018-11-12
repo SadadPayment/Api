d9279a5c0a82da8475574153633cdcb7ba0d316e
Tayeb-Ali <Mr.elteb@gmail.com>
2018-11-12 16:05:36 +0200

    add bank Account list and add

--- app/Http/Controllers/API/Users/ProfileApiController.php
+++ app/Http/Controllers/API/Users/ProfileApiController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\API\Users;
 
+use App\Model\Account\BankAccount;
 use App\Model\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ProfileApiController extends Controller
 {
@@ -19,4 +21,39 @@ class ProfileApiController extends Controller
         }
         return response()->json(['error' => true]);
     }
+
+    public function add_bank_account(Request $request)
+    {
+        try {
+            $token = JWTAuth::parseToken();
+            $user = $token->authenticate();
+            $save_account = new BankAccount($request->all());
+            $save_account->user_id = $user->id;
+            $save_account->save();
+            if ($save_account) {
+                return response()->json(true);
+            }
+            return response()->json(false);
+        }
+        catch (\Exception $exception){
+            return response()->json($exception);
+        }
+    }
+    public function get_bank_account(){
+        try {
+            $token = JWTAuth::parseToken();
+            $user = $token->authenticate();
+            $account = BankAccount::where('user_id', $user->id)->get();
+            if ($account){
+                return response()->json($account);
+            }
+            return response()->json('error');
+
+
+        }
+        catch (\Exception $exception){
+            return response()->json($exception);
+        }
+    }
+
 }
