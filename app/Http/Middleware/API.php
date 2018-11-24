ce089946d1db26a30e0921671f61ffda8b6a5110
Tayeb-Ali <Mr.elteb@gmail.com>
2018-11-24 23:58:12 +0200

    fix  JWT in User

--- app/Http/Middleware/API.php
+++ app/Http/Middleware/API.php
@@ -11,24 +11,29 @@ class API
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-
-        if ($request->has('token')) {
-            try {
-                $jwt = new JWTAuth();
-                $this->auth = $jwt->parseToken()->authenticate();
-                return $next($request);
-            } catch (JWTException $e) {
-                $response = array();
-                $response += ["error" => true];
-                $response += ["message" => "Authentication Error"];
-                return response()->json($response,"200");
-            }
+        if (!auth()->guard('api')->user()) {
+            return response()->json(['errorEn' => 'You must be user to do this',
+                'errorAr' => 'يجب ان تكون عضوء او مسجل الدخول حتي تم هذا الأجراء']);
         }
+        return $next($request);
+
+//        if ($request->has('token')) {
+//            try {
+//                $jwt = new JWTAuth();
+//                $this->auth = $jwt->parseToken()->authenticate();
+//                return $next($request);
+//            } catch (JWTException $e) {
+//                $response = array();
+//                $response += ["error" => true];
+//                $response += ["message" => "Authentication Error"];
+//                return response()->json($response,"200");
+//            }
+//        }
     }
 }
