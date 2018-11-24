6bb10e511972bc47ba5e77398e9b6811ae2b6197
Tayeb-Ali <Mr.elteb@gmail.com>
2018-11-25 00:02:54 +0200

    fix  JWT in User

--- routes/usersApi.php
+++ routes/usersApi.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
 
-Route::get('/profile', 'API\Users\ProfileApiController@get_user_profile_info')->middleware('jwt.auth');
-Route::post('/add_account', 'API\Users\ProfileApiController@add_bank_account')->middleware('jwt.auth');
-Route::get('/list_account', 'API\Users\ProfileApiController@get_bank_account')->middleware('jwt.auth');
-Route::get('/delete_account/{id}', 'API\Users\ProfileApiController@delete_bank_account')->middleware('jwt.auth');
+//Route::get('/profile', 'API\Users\ProfileApiController@get_user_profile_info')->middleware('jwt.auth');
+Route::get('/profile', 'API\Users\ProfileApiController@get_user_profile_info');
+Route::post('/add_account', 'API\Users\ProfileApiController@add_bank_account');
+Route::get('/list_account', 'API\Users\ProfileApiController@get_bank_account');
+Route::get('/delete_account/{id}', 'API\Users\ProfileApiController@delete_bank_account');
 //Route::post('/edit_account', 'API\Users\ProfileApiController@');
