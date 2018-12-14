a3208163d633e31ae37b31db0a0f6bb18f81e979
Tayeb-Ali <Mr.elteb@gmail.com>
2018-12-14 20:32:38 +0200

    fix

---
+++ app/Model/Agent/Reversal.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Reverse Transaction, can be performed by merchant,
+ * for example the customer who wants to return
+ * the purchase or wrong amount was inserted,
+ * or can be performed automatically from the client application
+ * in case of response timed out.
+ * */
+
+namespace App\Model\Agent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reversal extends Model
+{
+    //
+}
