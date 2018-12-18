439c66cec7939d04c0785d04c0036e33fac178ce
Tayeb-Ali <Mr.elteb@gmail.com>
2018-12-18 20:39:51 +0200

    Add Purchase Response Model Test #1.15

--- app/Http/Controllers/API/Payment/PurchaseUserControllerApi.php
+++ app/Http/Controllers/API/Payment/PurchaseUserControllerApi.php
@@ -22,7 +22,7 @@ class PurchaseUserControllerApi extends Controller
     /**
      * Store a newly created resource in storage.
      * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @return array|\Illuminate\Http\Response
      * @throws \Exception
      */
     public function userPurchase(Request $request)
@@ -65,85 +65,84 @@ class PurchaseUserControllerApi extends Controller
              * @Parameter phone
              * @ invoiceNumber
              * */
-            if ($userCart){
-                $Purchase = new PurchaseUser();
+            if (!$userCart) {
+                $r = ["message" => "خطا - حاول لاحقا", 'error' => true];
+                return $r;
+            }
+            $Purchase = new PurchaseUser();
             $Purchase->payment()->associate($payment);
             $Purchase->PAN = $userCart->PAN;
             $Purchase->save();
-        } else {
-            dd($userCart);
-        }
-        //Tran status
-        $transaction->status = "Send Request";
-        $transaction->save();
 
-        //Get PublicKey get Value Per Request
-        $publicKey = PublicKey::sendRequest();
-        if ($publicKey == false) {
-            $res = ["message" => "خطا - حاول لاحقا", 'error' => true];
-            return response()->json($res, 200);
-        }
-        $ipin = Functions::encript($publicKey, $uuid, $request->IPIN);
-
-        //$req = E15Model::requestBuild($transaction->id,$ipin,$type);
-        $response = PurchaseUser::sendRequest($transaction->id, $ipin, $request->id);
-        if ($response == false) {
-            $res = ["message" => "Some Error Found", 'error' => true];
-            return response()->json($res, 200);
-        }
-//            dd($response);
-        //اذا الرد = 0 معناه العملية تمت بنجاح
-        // اكبر من 0 او غيره  خطأ من ebs
-        if ($response->responseCode != 0) {
-            //response code in 29
             //Tran status
-            $transaction->status = "Ebs Error";
+            $transaction->status = "Send Request";
             $transaction->save();
-            $response_json = ["message" => "خطا- راجع البيانات المدخله", "ebs" => $response, 'error' => true];
-            return response()->json($response_json, 200);
+
+            //Get PublicKey get Value Per Request
+            $publicKey = PublicKey::sendRequest();
+            if ($publicKey == false) {
+                $res = ["message" => "خطا - حاول لاحقا", 'error' => true];
+                return response()->json($res, 200);
+            }
+            $ipin = Functions::encript($publicKey, $uuid, $request->IPIN);
+
+            //$req = E15Model::requestBuild($transaction->id,$ipin,$type);
+            $response = PurchaseUser::sendRequest($transaction->id, $ipin, $request->id);
+            if ($response == false) {
+                $res = ["message" => "Some Error Found", 'error' => true];
+                return response()->json($res, 200);
+            }
+//            dd($response);
+            //اذا الرد = 0 معناه العملية تمت بنجاح
+            // اكبر من 0 او غيره  خطأ من ebs
+            if ($response->responseCode != 0) {
+                //response code in 29
+                //Tran status
+                $transaction->status = "Ebs Error";
+                $transaction->save();
+                $response_json = ["message" => "خطا- راجع البيانات المدخله", "ebs" => $response, 'error' => true];
+                return response()->json($response_json, 200);
+            }
+
+            $basicResponse = Response::saveBasicResponse($transaction, $response);
+//            $paymentResponse = PaymentResponse::savePaymentResponse($basicResponse, $payment, $response);
+
+//            $purchase_response = self::save_purchase_response($paymentResponse, $Purchase, $response);
+
+            //Tran status
+            $transaction->status = "Done";
+            $transaction->save();
+            $json = array();
+
+            $responseData = [
+                'date' => $transaction->created_at->format('d-m-Y H:i'),
+                'id' => $transaction->id
+            ];// get Time and id of Request
+            $json += ['ebs' => $response];
+            $json += [
+                "error" => false,
+                "message" => "تم بنجاح",
+                "response" => $basicResponse, 'data' => $responseData];
+            return response()->json($json, 200);
+        } else {
+            $response = ["message" => "Request Must Be Json", 'error' => true];
+            return response()->json($response, 200);
         }
+    }
 
-        $basicResponse = Response::saveBasicResponse($transaction, $response);
-        $paymentResponse = PaymentResponse::savePaymentResponse($basicResponse, $payment, $response);
-
-        $purchase_response = self::save_purchase_response($paymentResponse, $Purchase, $response);
-
-        //Tran status
-        $transaction->status = "Done";
-        $transaction->save();
-        $json = array();
-
-        $responseData = [
-            'date' => $transaction->created_at->format('d-m-Y H:i'),
-            'id' => $transaction->id
-        ];// get Time and id of Request
-        $json += ['ebs' => $response];
-        $json += [
-            "error" => false,
-            "message" => "تم بنجاح",
-            "response" => $basicResponse, 'data' => $responseData];
-        return response()->json($json, 200);
-    } else
-{
-$response = ["message" => "Request Must Be Json", 'error' => true];
-return response()->json($response, 200);
-}
-}
-
-public
-static function save_purchase_response($paymentResponse, $purchase, $response)
-{
-    $purchase_response = new PurchaseResponse();
-    $purchase_response->PaymentResponse()->associate($paymentResponse);
-    $purchase_response->Purchase()->associate($purchase);
-    $purchase_response_save = (array)$response;
-//        dd($purchase_response_save);
-    $purchase_response->fill($purchase_response_save);
-//        $purchase_response->issuerTranFee = ;
-//        $purchase_response->fromAccount = ;
-//        $purchase_response->payment_response_id = ;
-//        $purchase_response->purchase_user_id = ;
-    $purchase_response->save();
-    return $purchase_response;
-}
+//    public static function save_purchase_response($paymentResponse, $purchase, $response)
+//    {
+//        $purchase_response = new PurchaseResponse();
+//        $purchase_response->PaymentResponse()->associate($paymentResponse);
+//        $purchase_response->Purchase()->associate($purchase);
+//        $purchase_response_save = (array)$response;
+////        dd($purchase_response_save);
+//        $purchase_response->fill($purchase_response_save);
+////        $purchase_response->issuerTranFee = ;
+////        $purchase_response->fromAccount = ;
+////        $purchase_response->payment_response_id = ;
+////        $purchase_response->purchase_user_id = ;
+//        $purchase_response->save();
+//        return $purchase_response;
+//    }
 }
